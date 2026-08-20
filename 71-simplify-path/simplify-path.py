0a47bfd7622a4675b8130d226ccb2775f24bab63
akshath-2006 <akshathpanoli30@gmail.com>
2026-08-21 01:24:08 +0530

    Time: 7 ms (11.14%) | Memory: 12.5 MB (59.24%) - LeetSync

---
+++ 71-simplify-path/simplify-path.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        s=[]
+        cur=''
+        for c in path+'/':
+            if c=="/":
+                if cur =="..":
+                    if s:s.pop()
+
+                elif cur!="" and cur!=".":
+                    s.append(cur)
+                cur=""
+            else:   
+                cur+=c
+        return "/"+"/".join(s)
