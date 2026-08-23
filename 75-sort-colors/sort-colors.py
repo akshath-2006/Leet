04571f55cb5003bc5c18dcf8bbdd3db2cda8d3d7
akshath-2006 <akshathpanoli30@gmail.com>
2026-08-23 17:41:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (91.23%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        c0, c1, c2 = nums.count(0), nums.count(1), nums.count(2)
+        nums[:c0] = [0] * c0
+        nums[c0:c0+c1] = [1] * c1
+        nums[c0+c1:] = [2] * c2
+        
+
+
+
+            
